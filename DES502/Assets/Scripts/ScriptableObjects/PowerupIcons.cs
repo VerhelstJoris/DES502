c9ebe4bfaca21ac213a10360a01e4c8398f06c83
Callum Mackay <mackaycallum99@gmail.com>
2019-03-21 13:35:54 +0000

    Added scriptable object to store powerup icons

---
+++ DES502/Assets/Scripts/ScriptableObjects/PowerupIcons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PowerupIconData", menuName = "Data/PowerupIcons", order = 1)]
+public class PowerupIcons : ScriptableObject
+{
+    public List<PowerupData> powerups;
+}
+
+[System.Serializable]
+public class PowerupData
+{
+    public string powerupName;
+    public Sprite inGameCollectableSprite;
+    //public Texture sidebarHUDTexture;
+    public Sprite sidebarHUDSprite;
+}
