59a250efefc383ce535a715b4167e2afdeb5365e
Callum Mackay <mackaycallum99@gmail.com>
2019-04-18 17:11:54 +0100

    Cleaned up RespawnPoint.cs

--- DES502/Assets/Scripts/Environment/RespawnPoint.cs
+++ DES502/Assets/Scripts/Environment/RespawnPoint.cs
@@ -4,46 +4,38 @@ using UnityEngine;
 
 public class RespawnPoint : MonoBehaviour
 {
-    private Animator _animator;
-    [HideInInspector]
-    public GameManager _GM;
- 
-
     [SerializeField]
     private RuntimeAnimatorController[] _animators;
 
+    [HideInInspector]
+    public GameManager _GM;
+    [HideInInspector]
     public bool _Active = false;
-
+    private Animator _animator;
     private PlayerData _playerData;
     private SpriteRenderer _renderer;
+
+    void Awake()
+    {
+        _GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
         _animator = this.GetComponent<Animator>();
         _renderer = this.GetComponent<SpriteRenderer>();
         _renderer.enabled = true;
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        
     }
 
     public void Activate(PlayerData data)
     {
         _playerData = data;
-        
         _animator.runtimeAnimatorController = _animators[data.skinID];
         _renderer.enabled = true;
         _Active = true;
         //_animator.Play("Respawn",-1,0);
         _animator.SetBool("Active", true);
-
     }
 
     public void RespawnFinished()
@@ -51,17 +43,16 @@ public class RespawnPoint : MonoBehaviour
         _renderer.enabled = false;
         _Active = false;
         _animator.SetBool("Active", false);
-
     }
 
     public void RespawnPlayer()
     {
         ObjectFactory.CreatePlayer(_playerData, this.transform.position, _GM._TeamSetup);
-
     }
 
     public bool IsCloseToOil(float maxDistance)
     {
+        // TODO: replace this with getting the layer bit from name instead of hard coding the value
         int hazardsLayer = 9;
         int layerMask = 1 << hazardsLayer;
         Collider2D[] overlappedHazards = Physics2D.OverlapCircleAll(transform.position, maxDistance, layerMask);
