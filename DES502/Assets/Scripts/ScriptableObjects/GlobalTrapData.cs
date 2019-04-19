aaa19fb6f3ab53e237c3b519f0c0ac9be2a0ea88
Callum Mackay <mackaycallum99@gmail.com>
2019-04-19 14:54:53 +0100

    Added fields in global trap data for cooldown blinking

--- DES502/Assets/Scripts/ScriptableObjects/GlobalTrapData.cs
+++ DES502/Assets/Scripts/ScriptableObjects/GlobalTrapData.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GlobalTrapDataAsset", menuName = "Data/Global Trap Data", order = 1)]
 public class GlobalTrapData : ScriptableObject
 {
+    [Tooltip("What color should the sprite renderer use for traps that are currently on cooldown?")]
     public Color _cooldownSpriteColor;
+    [Tooltip("How long (in seconds) before cooldown has ended should we start flashing between cooldown color and default?")]
+    public float _colorBlinkDuration = 1.5f;
+    [Range(0, 1)]
+    [Tooltip("How long (in seconds) should we wait in between flip flopping from default sprite color and cooldown sprite color while blinking?")
+    public float _colorBlinkWaitDuration = 0.2f;
 }
