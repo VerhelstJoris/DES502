e5e57167b664ce19160b0734f4cafa5f942a1f3d
Callum Mackay <mackaycallum99@gmail.com>
2019-04-20 19:26:21 +0100

    Added rock trap

---
+++ DES502/Assets/Scripts/Environment/RockTrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockTrap : Trap
+{
+    [Header("Rock Trap Specific")]
+    [SerializeField] [Tooltip("Reference to the rock prefab.")]
+    private GameObject _rockPrefab;
+    [SerializeField] [Tooltip("Reference to the spawn location child object.")]
+    private GameObject _rockSpawnLocation;
+
+    public override void Awake()
+    {
+        _spriteRenderer = _rockSpawnLocation.GetComponent<SpriteRenderer>();
+    }
+
+    public override void Trigger(CharacterController player)
+    {
+        Debug.Log("SPAWN ROCK");
+        BeginCooldownTimer();
+    }
+}
