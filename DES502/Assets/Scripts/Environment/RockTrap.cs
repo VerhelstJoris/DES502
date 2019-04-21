6ea72d56269dc929e5f934e3fc1e92fd7397d3c8
Callum Mackay <mackaycallum99@gmail.com>
2019-04-21 03:01:50 +0100

    Added rock fall trap

--- DES502/Assets/Scripts/Environment/RockTrap.cs
+++ DES502/Assets/Scripts/Environment/RockTrap.cs
@@ -9,16 +9,19 @@ public class RockTrap : Trap
     private GameObject _rockPrefab;
     [SerializeField] [Tooltip("Reference to the spawn location child object.")]
     private GameObject _rockSpawnLocation;
+    private Vector3 _rockSpawnPosition;
 
     public override void Awake()
     {
         _spriteRenderer = _rockSpawnLocation.GetComponent<SpriteRenderer>();
         SetCooldownColors(true);
+        _rockSpawnPosition = _rockSpawnLocation.transform.position;
     }
 
     public override void Trigger(CharacterController player)
     {
         Debug.Log("SPAWN ROCK");
+        GameObject newRock = Instantiate(_rockPrefab, _rockSpawnPosition, Quaternion.identity);
         BeginCooldownTimer();
     }
 }
